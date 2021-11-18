a2692c0386a7e10ad99994ef169797866b929b2b
smita Raj <smita.raj12@gmail.com>
2021-11-18 14:41:08 -0800

    add validation

--- AnimalShelter/Models/Cat.cs
+++ AnimalShelter/Models/Cat.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Cat
     {
         public int CatId { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
+        [Range(0, 20, ErrorMessage = "Age must be between 0 and 20.")]
         public int Age { get; set; }
+        [Required]
         public string Type { get; set; }
     }
 }
