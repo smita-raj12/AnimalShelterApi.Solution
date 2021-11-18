312c1fff6517798d50491daeb892d8ab7420ce5c
smita Raj <smita.raj12@gmail.com>
2021-11-18 10:14:54 -0800

    add animal context file

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cat> Cats { get; set; }
+
+        public DbSet<Dog> Dogs { get; set; }
+        public DbSet<OtherAnimal> OtherAnimals { get; set; }
+    }
+}
