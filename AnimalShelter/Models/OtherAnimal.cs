3228ccff88e59de84f5b73d6ffe0c5931986c25e
smita Raj <smita.raj12@gmail.com>
2021-11-18 10:10:33 -0800

    add all class on model file

---
+++ AnimalShelter/Models/OtherAnimal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+    public class OtherAnimal
+    {
+        public int OtherAnimalId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Type { get; set; }
+    }
+}
